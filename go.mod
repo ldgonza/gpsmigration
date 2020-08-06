d923c291fb519b35887c3981ecf55a39dd4e9217
Leonardo González <ldgonza@gmail.com>
2020-08-06 10:39:38 -0300

    Parse connection properties

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gitlab.com/simpliroute/gps-migration-to-json
 
 go 1.13
+
+require github.com/magiconair/properties v1.8.1
--- work/work.go
+++ work/work.go
@@ -3,10 +3,15 @@ package work
 import (
 	"fmt"
 	"time"
+
+	"github.com/magiconair/properties"
 )
 
 // Work processes the migration.
 func Work(i int) {
 	time.Sleep(time.Second)
 	fmt.Println(i)
+	p := properties.MustLoadFile("connection.properties", properties.UTF8)
+	x, _ := p.Get("x")
+	fmt.Println(x)
 }
